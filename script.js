f2f6540859175a17bd0abd6cc486fe710a9df6d3
something-natural <tsglow@naver.com>
2024-11-14 13:54:05 +0900

    fix: changed basic structure, added test functions

--- script.js
+++ script.js
@@ -49,48 +49,89 @@ const resultParagraph = document.getElementById("result_p")
 
 
 // change result div
-const showResult = (text, judge) => resultParagraph.innerText = `${judge} US number: ${text}`;
-const eraseResult = () => resultParagraph.innerText = "";
+const makeResultText = (text, judge = "none") => judge === "none" ? resultParagraph.innerText = "" : resultParagraph.innerText = `${judge} US number: ${text}`;
+
+// evaluate
 
 //parser
-const parse = text => {
-    // you should accep               t (,), space(replace to underbar), dash only
+const parse = text => {  
+    
+    const invalidReason = []; // object to store invalid reasons
+
+    //regex
+    const availSignRegex = /[\)\(\s-]/g;
+    
+    //coverter
+    const textToNums = text => text.replace(availSignRegex, "").split("").map(el => parseInt(el));    
+
+
+    // now evaluate
+    /*
+    like this?
+    const test1 = (){};
+    const test2 = (){};
+    const test3 = (){};
+    const tests = { test1, test2, test3, test4...};
+    let testnumber = 1;
+    
+    invalidReason.length === 0 ? makeResultText(text, "Valid") : makeResultText(text, "invalid")
+    */
+
+    // checkLength  
+    const test1 = text => {
+        const numsArray = textToNums(text);
+        console.log(numsArray.length);
+        if (numsArray.length !== 11 && numsArray.length !== 10){
+            invalidReason.push("lengthCheck_failed");
+        }
+    }    
+    // check invalid letter
+    const test2 = text => {
+        const numsArray = textToNums(text);
+        console.log(numsArray);
+        const isThereNan = numsArray.some((el) => Number.isNaN(el));        
+        if (isThereNan){
+            invalidReason.push("lettercheck_failed");
+        }
+    }
+
+    // check letter index
     // possible index for '(' = 0,2 
     // possible index for ')' = 4,5
     // possible index for '-' = 3,4,7,8
     // possible index for space = 1, 5, 7, 9
-    
-    
-        
-    //regex
-    const availSignRegex = /[\)\(\s-]/g;
-        
-    //evaluate
-    // 1. check whether unavailable sign used or not
-    const someNaN = nums => nums.some(el => Number.isNaN(el)); // don't use typeof(el) === "number", because typeof NaN is number!
-    
 
-    //result msg    
+    const tests = {
+        test1,
+        test2
+    };
     
+    let testnumber = 1;
+    while ( testnumber <= Object.keys(tests).length ){
+        tests[`test${testnumber}`](text);
+        testnumber += 1;        
+        console.log("testlength", Object.keys(tests).length);
+    }    
 
-    //parse
-    const textToNums = text.replace(availSignRegex, "").split("").map(el => parseInt(el));
-    console.log(textToNums);    
-    
-    return someNaN(textToNums) ? showResult(text,"Invalid") : showResult(text,"Valid");
-        
+    return invalidReason.length === 0 ? "Valid" : "Invalid"
 }
 
 
-
-
+const getResult = text => {
+    if ( text === ""){
+        makeResultText(text);
+    } else {
+        makeResultText(text,parse(text));
+    }
+        
+}
 
 // Button Event Listeners
 checkBtn.addEventListener("click", () => {
-    parse(getInputValue.value);    
+    getResult(getInputValue.value);    
 })
 
 clearBtn.addEventListener("click", () => {
     getInputValue.value = "";    
-    eraseResult();
+    getResult(getInputValue.value);        
 })
